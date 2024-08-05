518faa6f80aed5b2d212f34e60c0c31feba76049
Azlan Ibrahim <azlibdar@gmail.com>
2024-08-05 21:41:30 +0530

    removed useToggle from index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ export { default as useFavicon } from "./hooks/useFavicon";
 export { default as useDefault } from "./hooks/useDefault";
 export { default as usePrevious } from "./hooks/usePrevious";
 export { default as useCounter } from "./hooks/useCounter";
-export { default as useToggle } from "./hooks/useToggle";
 export { default as useCopyToClipboard } from "./hooks/useCopyToClipboard";
 export { default as usePreferredDark } from "./hooks/usePreferredDark";
 export { default as useLockScroll } from "./hooks/useLockScroll";
